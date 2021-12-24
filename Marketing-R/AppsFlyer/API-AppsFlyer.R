ed040a80c313323aaef9707999fadd4f689c2245
Oleg_Loginov <79114766+Oleg-Loginov-analyst@users.noreply.github.com>
2021-12-24 17:44:48 +0300

    Add files via upload

---
+++ Marketing-R/AppsFlyer/API-AppsFlyer.R
@@ -0,0 +1,62 @@
+# Подключаем пакеты
+
+library(rappsflyer)
+library(remotes)
+library(bigrquery)
+library(DBI)
+library(RJDBC)
+library(dbplyr)
+library(tidyverse)
+library(openxlsx)
+library(RODBC)
+library(odbc)
+library(remotes)
+library(bigrquery)
+library(DBI)
+library(RJDBC)
+
+# Электронная почта, к которой привязан токен
+options(gargle_oauth_email = "loginovod90@gmail.com")
+
+# Устанавливаем токен
+af_set_api_token("7a4463d4-be8d-40e2-9b83-c7f4d9bb8894")
+
+# Получение агрегированных данных по дням и по географии
+geo_by_date_report <- af_get_aggregate_data(
+  date_from         = "2021-01-01",
+  date_to           = Sys.Date() - 1,
+  report_type       = "geo_by_date_report",
+  additional_fields = c("keyword_id",
+                        "store_reinstall",
+                        "deeplink_url",
+                        "oaid",
+                        "install_app_store",
+                        "contributor1_match_type",
+                        "contributor2_match_type",
+                        "contributor3_match_type",
+                        "match_type"),
+  app_id            = "ru.name.name") # id вашего приложения в AppsFlyer
+
+# Убираем NA, переименовываем колонки, приводим дату к нужному формату 
+geo_by_date_report[is.na(geo_by_date_report)] <- 0
+geo_by_date_report <- geo_by_date_report[, -3]
+geo_by_date_report <- rename(geo_by_date_report, Medium_source        = `Media Source (pid)`)
+geo_by_date_report <- rename(geo_by_date_report, Campaign             = `Campaign (c)`)
+geo_by_date_report <- rename(geo_by_date_report, Conversion_rate      = `Conversion Rate`)
+geo_by_date_report <- rename(geo_by_date_report, Loyal_users          = `Loyal Users`)
+geo_by_date_report <- rename(geo_by_date_report, Loyal_users_installs = `Loyal Users/Installs`)
+geo_by_date_report <- rename(geo_by_date_report, Total_revenue        = `Total Revenue`)
+geo_by_date_report <- rename(geo_by_date_report, Total_cost           = `Total Cost`)
+geo_by_date_report <- rename(geo_by_date_report, Average_eCPI         = `Average eCPI`)
+geo_by_date_report$Date = as.Date(geo_by_date_report$Date)
+
+# Подключаемся в БД Google BigQuery
+
+# Загружаем данные в BigQuery
+# Агрегированные данные по дням и по географии
+bq_table(project = "flawless-age-310013",                  # название проекта
+         dataset = "aflyer_dataset",                       # название датасета (набор данных)
+         table   = "geo_camp_stat") %>%                    # название таблицы
+  bq_table_upload(values = geo_by_date_report,             # загружаемый датасет из вышесозданного кода
+                  create_disposition = "CREATE_IF_NEEDED", # Создание ноовый таблицы
+                  write_disposition  = "WRITE_TRUNCATE")   # Перезаписать данные в таблице
