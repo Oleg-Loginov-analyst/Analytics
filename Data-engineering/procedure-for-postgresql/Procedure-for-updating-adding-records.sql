fb792dc99539511c5d40fde520858eeef79e30b9
Oleg_Loginov <loginovod90@gmail.com>
2023-05-15 10:22:06 +0300

    Update Procedure-for-updating-adding-records.sql

--- Data-engineering/procedure-for-postgresql/Procedure-for-updating-adding-records.sql
+++ Data-engineering/procedure-for-postgresql/Procedure-for-updating-adding-records.sql
@@ -1,23 +1,35 @@
 -- Создание процедуры
-CREATE OR REPLACE FUNCTION update_test_data() RETURNS VOID AS $$
-DECLARE
-  max_date DATE;
-BEGIN
+create or replace function update_test_data() returns void as $$
+declare
+  max_date date;
+begin
   -- Получение максимальной даты в таблице
-  SELECT MAX(updated_at) INTO max_date FROM test_data;
+  select
+    max(updated_at) into max_date
+  from
+    test_data
+  ;
   
   -- Обновление записей с date_to = '31.12.9999' на актуальную дату
-  UPDATE test_data SET date_to = max_date WHERE date_to = '9999-12-31';
+  update test_data set date_to = max_date where date_to = '9999-12-31';
   
   -- Вставка новых записей
-  INSERT INTO test_data (id, rev, created_at, updated_at)
-  SELECT id, rev, created_at, updated_at
-  FROM (
-    SELECT DISTINCT ON (id) id, rev, created_at, updated_at
-    FROM test_data
-    WHERE updated_at > max_date
-    ORDER BY id, updated_at DESC
-  ) AS new_records;
+  insert into test_data (id, rev, created_at, updated_at)
+  select
+    id
+    ,rev
+    ,created_at
+    ,updated_at
+  from (
+    select distinct on (id) id, rev, created_at, updated_at
+    from
+      test_data
+    where
+      updated_at > max_date
+    order by
+      id
+      ,updated_at desc
+  ) as new_records;
 END;
-$$ LANGUAGE plpgsql
+$$ language plpgsql
 ;
